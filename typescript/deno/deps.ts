9383538b63ca952d7f67280390047ca056350a3e
Heechul Ryu <ryuhcii@gmail.com>
2022-09-29 15:33:02 -0700

    Add typescript/deno

---
+++ typescript/deno/deps.ts
@@ -0,0 +1,2 @@
+// read at https://deno.land/manual@v1.26.0/examples/manage_dependencies
+export { assertEquals } from "https://deno.land/std@0.158.0/testing/asserts.ts";
---
+++ typescript/deno/example_test.ts
@@ -0,0 +1,6 @@
+// read at https://deno.land/manual@v1.26.0/testing
+import { assertEquals } from "./deps.ts";
+
+Deno.test("example test", () => {
+  assertEquals(1, 1);
+});
